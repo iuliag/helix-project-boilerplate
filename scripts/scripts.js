69b128e885e9fc91ce65201c447bc1843acde751
Iulia Grumaz <grumaz@adobe.com>
2023-04-06 11:48:52 +0300

    push to adobe data layer

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -118,6 +118,23 @@ async function loadLazy(doc) {
 
   // loadLaunch(() => analyticsTrackPageViews(document));
   loadLaunch();
+
+  // faking a data layer change
+  if (!window.adobeDataLayer) {
+    window.adobeDataLayer = [];
+  }
+  window.adobeDataLayer.push({
+    event: 'Form Complete',
+    eventInfo: {
+      form: {
+        formId: '12345',
+      },
+      experiment: {
+        experimentId: 'pricing',
+        experimentVariant: 'challenger-1',
+      },
+    },
+  });
 }
 
 /**
