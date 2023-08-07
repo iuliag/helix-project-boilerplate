a393d43593279f283ac2f6b608a4ba9d55a9082a
Iulia Grumaz <grumaz@adobe.com>
2023-08-07 11:54:11 +0300

    chore: add launch script delayed

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './lib-franklin.js';
+import { loadScript, sampleRUM } from './lib-franklin.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+await loadScript('https://assets.adobedtm.com///launch--development.min.js', { async: true });
